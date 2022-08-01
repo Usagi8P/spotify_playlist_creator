78243cae41b78877728f79e5c6b5564c686d6ce3
Usagi <ph7stein@hotmail.com>
2022-08-01 21:59:53 +0200

    Get contents of playlists

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import logging
+import sys
+from pprint import pprint
 
 
 def logger_setup() -> None:
@@ -41,11 +43,53 @@ def set_env_variables() -> None:
     if 'SPOTIPY_REDIRECT_URI' not in os.environ:
         os.environ['SPOTIPY_REDIRECT_URI']=secrets['SPOTIPY_REDIRECT_URI']
         logging.info('Set SPOTIPY_REDIRECT_URI')
+
+
+def get_playlists() -> list[str]:
+    # Reads playlist codes from the secrets folder and loads them into a list
+    playlists: list[str] = []
+
+    with open('secrets/playlists.txt','r') as f:
+        lines = f.readlines() 
+
+        for line in lines:
+            playlists.append(line[line.find('=')+1:-1])
     
+    return playlists
+
+
+def get_songs_in_playlist(sp,pl_id):
+    offset = 0
+
+    response = sp.playlist_items(pl_id,
+                                offset=offset,
+                                fields='items.track.id,total',
+                                additional_types=['track'])
+
+    return response
+
+
+def create_total_track_list(sp):
+    playlists: list[str] = get_playlists()
+
+    track_ids: list[str] = []
+    for playlist in playlists:
+        print(playlist)
+        response = get_songs_in_playlist(sp,playlist)
+        pprint(response['items'])
+
+        for item in response['items']:
+            track_ids.append(item['track']['id'])
+
+    return track_ids
+
 
 def main():
     logger_setup()
     set_env_variables()
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    pprint(create_total_track_list(sp))
 
 
 if __name__ == "__main__":
