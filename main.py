6bfce0bf58c7c9a04d5226f3fdd76b94dd5f101f
Usagi <ph7stein@hotmail.com>
2022-08-02 22:12:09 +0200

    Create new playlist and fill with songs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import os
 import logging
-import sys
 from pprint import pprint
 
 
@@ -53,7 +52,9 @@ def get_playlists() -> list[str]:
         lines = f.readlines() 
 
         for line in lines:
-            playlists.append(line[line.find('=')+1:-1])
+            playlist_id = line[line.find('=')+1:-1]
+            if playlist_id:
+                playlists.append(playlist_id)
     
     return playlists
 
@@ -69,14 +70,12 @@ def get_songs_in_playlist(sp,pl_id):
     return response
 
 
-def create_total_track_list(sp):
+def create_total_track_list(sp) -> list[str]:
     playlists: list[str] = get_playlists()
 
     track_ids: list[str] = []
     for playlist in playlists:
-        print(playlist)
         response = get_songs_in_playlist(sp,playlist)
-        pprint(response['items'])
 
         for item in response['items']:
             track_ids.append(item['track']['id'])
@@ -84,12 +83,63 @@ def create_total_track_list(sp):
     return track_ids
 
 
+def create_new_playlist(sp_personal) -> str:
+    with open('secrets/username.txt','r') as f:
+        username = f.readline()
+
+    new_playlist = sp_personal.user_playlist_create(username,'Full Playlist')
+    
+    with open('secrets/playlist_id.txt','w') as f:
+        f.write(new_playlist['id'])
+
+    return new_playlist['id']
+
+
+def get_new_playlist_id(sp_personal) -> str:
+    try:
+        with open('secrets/playlist_id.txt','r') as f:
+            playlist_id = f.readline()
+
+            if playlist_id:
+                return playlist_id
+
+            if not playlist_id:
+                return create_new_playlist(sp_personal)
+
+    except:
+        return create_new_playlist(sp_personal)
+
+
 def main():
     logger_setup()
     set_env_variables()
     client_credentials_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    pprint(create_total_track_list(sp))
+    scope='playlist-modify-public'
+    sp_personal = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    tracklist = (create_total_track_list(sp))
+    set_tracklist = set(tracklist)
+
+    new_playlist_id = get_new_playlist_id(sp_personal)
+
+    final_tracklist = list(set_tracklist)
+    n_tracks = len(final_tracklist)
+    
+    if n_tracks <= 100:
+        sp_personal.playlist_replace_items(new_playlist_id,final_tracklist)
+    else:
+        uploaded_tracks: int = 0
+        sp_personal.playlist_replace_items(new_playlist_id,final_tracklist[uploaded_tracks:uploaded_tracks+99])
+        uploaded_tracks += 99
+
+        while uploaded_tracks + 99 <= n_tracks - 1:
+            sp_personal.playlist_add_items(new_playlist_id,final_tracklist[uploaded_tracks:uploaded_tracks+99])
+            uploaded_tracks += 99
+        
+        if uploaded_tracks < n_tracks - 1:
+            sp_personal.playlist_add_items(new_playlist_id,final_tracklist[uploaded_tracks:n_tracks-1])
+
 
 
 if __name__ == "__main__":
